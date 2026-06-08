7324005287d84cb0d4e33935540588c79cf51757
Saravana Kumar <saravanakumar@Saravanas-MacBook-Air.local>
2026-06-08 08:18:46 +0530

    update - 2026-06-08_08:18

--- backend/routers/purchases.py
+++ backend/routers/purchases.py
@@ -2,14 +2,146 @@ from fastapi import APIRouter, Form
 from core.dependencies import engine_mysql
 from sqlalchemy import text
 import pandas as pd
-
+import json
 
 router = APIRouter()
 
-@router.post("/details")
-def purchase_details(vno: str = Form(...), vdt: str = Form(...)):
 
-    sql = text("""
+@router.post("/update")
+def update_purchase(
+    purchase_id: str = Form(...),
+    purchase_date: str = Form(...),
+    vendor: str = Form(...),
+    expenses: float = Form(...),
+    details: str = Form(...),
+):
+
+    details = json.loads(details)
+
+    purchase_sql = text(
+        """
+        INSERT INTO yora_purchases
+        (
+            purchase_id,
+            purchase_date,
+            vendor,
+            expenses
+        )
+        VALUES
+        (
+            :purchase_id,
+            :purchase_date,
+            :vendor,
+            :expenses
+        )
+        ON DUPLICATE KEY UPDATE
+            vendor = VALUES(vendor),
+            expenses = VALUES(expenses)
+    """
+    )
+
+    purchase_detail_sql = text(
+        """
+        INSERT INTO yora_purchases_details
+        (
+            purchase_id,
+            purchase_date,
+            stock_item,
+            qty,
+            carton,
+            qty_per_carton,
+            item_count,
+            item_no,
+            list_price,
+            cost_price,
+            expenses,
+            landing_cost,
+            cost_with_gst,
+            gst
+        )
+        VALUES
+        (
+            :purchase_id,
+            :purchase_date,
+            :stock_item,
+            :qty,
+            :carton,
+            :qty_per_carton,
+            :item_count,
+            :item_no,
+            :list_price,
+            :cost_price,
+            :expenses,
+            :landing_cost,
+            :cost_with_gst,
+            :gst
+        )
+        ON DUPLICATE KEY UPDATE
+            qty = VALUES(qty),
+            carton = VALUES(carton),
+            qty_per_carton = VALUES(qty_per_carton),
+            item_count = VALUES(item_count),
+            item_no = VALUES(item_no),
+            list_price = VALUES(list_price),
+            cost_price = VALUES(cost_price),
+            expenses = VALUES(expenses),
+            landing_cost = VALUES(landing_cost),
+            cost_with_gst = VALUES(cost_with_gst),
+            gst = VALUES(gst)
+    """
+    )
+
+    detail_params = []
+
+    for detail in details:
+        detail_params.append(
+            {
+                "purchase_id": purchase_id,
+                "purchase_date": purchase_date,
+                "stock_item": detail["stock_item"],
+                "qty": detail["qty"],
+                "carton": detail["carton"],
+                "qty_per_carton": detail["qty_per_carton"],
+                "item_count": detail["item_count"],
+                "item_no": detail["item_no"],
+                "list_price": detail["rate"],
+                "cost_price": detail["cost_price"],
+                "expenses": detail["expenses"],
+                "landing_cost": detail["landing_cost"],
+                "cost_with_gst": detail["cost_with_gst"],
+                "gst": detail["gst"],
+            }
+        )
+
+    with engine_mysql.begin() as connection:
+
+        connection.execute(
+            purchase_sql,
+            {
+                "purchase_id": purchase_id,
+                "purchase_date": purchase_date,
+                "vendor": vendor,
+                "expenses": expenses,
+            },
+        )
+
+        connection.execute(
+            purchase_detail_sql,
+            detail_params,
+        )
+
+        connection.commit()
+
+    return {
+        "status": "success",
+        "message": "Purchase updated successfully!",
+    }
+
+
+@router.post("/details")
+def purchase_details(purchase_id: str = Form(...), purchase_date: str = Form(...)):
+    sql = text(
+        """
             SELECT
                 p.*,
                 i.gst,
@@ -17,23 +149,57 @@ def purchase_details(vno: str = Form(...), vdt: str = Form(...)):
             FROM purchases p
             INNER JOIN items i
                 ON p.stock_item = i.stock_item
-            WHERE p.vno = :vno
-            AND DATE(p.vdt) = :vdt
+            WHERE p.vno = :purchase_id
+            AND DATE(p.vdt) = :purchase_date
             ORDER BY p.itemno
-        """)
+        """
+    )
 
     with engine_mysql.connect() as connection:
-        result = connection.execute(sql, {"vno": vno, "vdt": vdt}).fetchall()
+        result = connection.execute(
+            sql, {"purchase_id": purchase_id, "purchase_date": purchase_date}
+        ).fetchall()
 
     purchase_details = pd.DataFrame(result)
-    purchase_details["CARTON"] = purchase_details.apply(lambda x: x["CARTON_BOX"] if x["CARTON_BOX"] > 0 else x["QTY"] / 72, axis=1)
-    purchase_details["COST_PRICE"] = purchase_details["VALUE"] / purchase_details["QTY"]
-    purchase_details["EXPENSES"] = 0
-    purchase_details["LANDING_COST"] = purchase_details["COST_PRICE"]+ purchase_details["EXPENSES"]
-    purchase_details["GST"] = purchase_details["gst"]
-    purchase_details["COST_WITH_GST"] = (
-        purchase_details["LANDING_COST"] * (1 + purchase_details["GST"] / 100)
+    purchase_details.rename(
+        columns={
+            "CARTON_BOX": "carton_box",
+            "CARTON_BOX_QTY": "qty_per_carton",
+            "ITEMCOUNT": "item_count",
+            "ITEMNO": "item_no",
+            "LEDGER_NAME": "vendor",
+            "QTY": "qty",
+            "VALUE": "value",
+            "GST": "gst",
+            "STOCK_ITEM": "stock_item",
+            "RATE": "rate",
+            "COST_PRICE": "cost_price",
+            "LANDING_COST": "landing_cost",
+            "COST_WITH_GST": "cost_with_gst",
+            "VNO": "purchase_id",
+            "VDT": "purchase_date",
+        },
+        inplace=True,
     )
+    purchase_details.drop(
+        columns=["ALT_QTY", "PACKING", "BRAND", "REPNAME"], inplace=True
+    )
+
+    purchase_details["carton"] = purchase_details.apply(
+        lambda x: x["carton_box"] if x["carton_box"] > 0 else x["qty"] / 72, axis=1
+    )
+
+    purchase_details["cost_price"] = purchase_details["value"] / purchase_details["qty"]
+    purchase_details["expenses"] = 0
+    purchase_details["landing_cost"] = (
+        purchase_details["cost_price"] + purchase_details["expenses"]
+    )
+    purchase_details["gst"] = purchase_details["gst"]
+    purchase_details["cost_with_gst"] = purchase_details["landing_cost"] * (
+        1 + purchase_details["gst"] / 100
+    )
+    print(purchase_details.columns)
+
     purchase_details = purchase_details.to_dict(orient="records")
 
     return {
@@ -42,24 +208,27 @@ def purchase_details(vno: str = Form(...), vdt: str = Form(...)):
         "data": purchase_details,
     }
 
-@router.post("/pending-costing-bills")
-@router.get("/pending-costing-bills")
-def pending_costing_bills():
-    sql = text("""
-        SELECT p.VNO, date(p.vdt) as VDT, p.LEDGER_NAME as VENDOR, sum(p.qty) as QTY
+
+@router.post("/pending-purchase-bills")
+@router.get("/pending-purchase-bills")
+def pending_purchase_bills():
+    sql = text(
+        """
+        SELECT p.vno as purchase_id,
+            date(p.vdt) as purchase_date, p.ledger_name as vendor, sum(p.qty) as qty
             FROM purchases p
         WHERE NOT EXISTS (
             SELECT 1
-                FROM purchase_costing pc
-            WHERE pc.vno = p.vno
-                AND pc.vdt = p.vdt
+                FROM yora_purchases pc
+            WHERE pc.purchase_id = p.vno
+                AND pc.purchase_date = p.vdt
         )
-        GROUP BY vno, vdt, ledger_name;
-    """)
+        GROUP BY p.vno, p.vdt, ledger_name;
+    """
+    )
 
     with engine_mysql.connect() as connection:
         result = connection.execute(sql).fetchall()
-
 
     pending_bills = pd.DataFrame(result)
     pending_bills = pending_bills.to_dict(orient="records")
--- frontend/src/pages/transactions/PurchaseCosting.jsx
+++ frontend/src/pages/transactions/PurchaseCosting.jsx
@@ -7,21 +7,21 @@ const PurchaseCosting = () => {
   const { api } = useContext(AuthContext);
   const [selectedPurchase, setSelectedPurchase] = useState(null);
   const [showPendingBills, setShowPendingBills] = useState(false);
-  const [costingDetails, setCostingDetails] = useState(null);
-  const [costing, setCosting] = useState({
-    COSTING_ID: "NEW",
-    VNO: "",
-    VDT: "",
-    VENDOR: "",
-    EXPENSES: 0,
-    DETAILS: {},
+  const [purchaseDetails, setPurchaseDetails] = useState(null);
+  const [purchase, setPurchase] = useState({
+    action: "NEW",
+    purchase_id: "",
+    purchase_date: "",
+    vendor: "",
+    expenses: 0,
+    details: null,
   });
 
   useEffect(() => {
     if (selectedPurchase) {
       const fd = new FormData();
-      fd.append("vno", selectedPurchase.VNO);
-      fd.append("vdt", selectedPurchase.VDT);
+      fd.append("purchase_id", selectedPurchase.purchase_id);
+      fd.append("purchase_date", selectedPurchase.purchase_date);
       fetch(`${api}/purchases/details`, {
         method: "POST",
         body: fd,
@@ -29,7 +29,13 @@ const PurchaseCosting = () => {
         .then((response) => response.json())
         .then((data) => {
           if (data.status === "success") {
-            setCostingDetails(data.data);
+            setPurchase({
+              ...purchase,
+              purchase_id: selectedPurchase.purchase_id,
+              purchase_date: selectedPurchase.purchase_date,
+              vendor: selectedPurchase.vendor,
+            });
+            setPurchaseDetails(data.data);
           }
         });
     }
@@ -38,27 +44,54 @@ const PurchaseCosting = () => {
   const handleUpdateExpenses = (e) => {
     const transportCost = parseFloat(e.target.value) || 0;
 
-    if (costingDetails) {
-      const totalCarton = costingDetails.reduce(
-        (sum, item) => sum + item.CARTON,
+    if (purchaseDetails) {
+      const totalCarton = purchaseDetails.reduce(
+        (sum, item) => sum + item.carton,
         0,
       );
       const costPerCarton = totalCarton > 0 ? transportCost / totalCarton : 0;
 
-      const updatedDetails = costingDetails.map((item) => {
-        const additionalExpense = (costPerCarton / item.QTY) * item.CARTON;
+      const updatedDetails = purchaseDetails.map((item) => {
+        const additionalExpense = (costPerCarton / item.qty) * item.carton;
         return {
           ...item,
-          EXPENSES: additionalExpense,
-          LANDING_COST: item.COST_PRICE + additionalExpense,
-          COST_WITH_GST: item.LANDING_COST * (1 + item.GST / 100),
+          expenses: additionalExpense,
+          landing_cost: item.cost_price + additionalExpense,
+          cost_with_gst:
+            (item.cost_price + additionalExpense) * (1 + item.gst / 100),
         };
       });
 
-      setCostingDetails(updatedDetails);
+      setPurchase({
+        ...purchase,
+        expenses: transportCost,
+        details: updatedDetails,
+      });
+
+      setPurchaseDetails(updatedDetails);
     }
   };
 
+  const handleUpdatePurchase = () => {
+    const fd = new FormData();
+    fd.append("purchase_id", purchase.purchase_id);
+    fd.append("purchase_date", purchase.purchase_date);
+    fd.append("vendor", purchase.vendor);
+    fd.append("expenses", purchase.expenses);
+    fd.append("details", JSON.stringify(purchaseDetails));
+
+    console.log(purchaseDetails);
+    fetch(`${api}/purchases/update`, {
+      method: "POST",
+      body: fd,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // if (data.status === "success") {
+        //   setSelectedPurchase(null);
+        // }
+      });
+  };
   return (
     <div className="flex flex-col items-center justify-center w-full">
       {showPendingBills && (
@@ -77,7 +110,7 @@ const PurchaseCosting = () => {
             <>
               <input
                 type="button"
-                value="Select From Purchase"
+                value="Import From Tally Data"
                 onClick={() => {
                   setShowPendingBills(true);
                 }}
@@ -85,7 +118,7 @@ const PurchaseCosting = () => {
               <input type="button" value="Select From Costing" />
             </>
           )}
-          {selectedPurchase && costingDetails && (
+          {selectedPurchase && purchaseDetails && (
             <>
               <input
                 type="button"
@@ -94,23 +127,17 @@ const PurchaseCosting = () => {
                 className="mb-2"
               />
               <div className="w-full rounded-md grid grid-cols-2 gap-2 items-center justify-between text-sm border border-gray-600 px-2 py-2 bg-gray-700/50">
-                <span className="text-start font-bold">
-                  Purchase Costing No
-                </span>
-                <span className="text-end font-bold">
-                  {selectedPurchase && "NEW"}
-                </span>
-                <span className="text-start font-bold">Voucher Date</span>
+                <span className="text-start font-bold">Purchase ID</span>
                 <span className="font-bold text-end">
-                  {moment(selectedPurchase.VDT).format("DD-MM-YYYY")}
+                  {selectedPurchase.purchase_id}
                 </span>
-                <span className="text-start font-bold">Voucher No</span>
+                <span className="text-start font-bold">Purchase Date</span>
                 <span className="font-bold text-end">
-                  {selectedPurchase.VNO}
+                  {moment(selectedPurchase.purchase_date).format("DD-MM-YYYY")}
                 </span>
                 <span className="text-start font-bold">Vendor</span>
                 <span className="font-bold text-end">
-                  {selectedPurchase.VENDOR}
+                  {selectedPurchase.vendor}
                 </span>
               </div>
               <div className="w-full rounded-md grid grid-cols-[3fr_1fr] gap-2 items-center justify-baseline text-sm border border-gray-600 px-2 py-1 bg-gray-700/50">
@@ -124,7 +151,12 @@ const PurchaseCosting = () => {
                 />
               </div>
               <div className="col-span-2 flex justify-end px-2 py-1">
-                <button className="btn btn-success">Update</button>
+                <button
+                  onClick={handleUpdatePurchase}
+                  className="btn btn-success"
+                >
+                  Update
+                </button>
               </div>
               <div>
                 <table className="w-full rounded-2xl text-sm border border-gray-600">
@@ -141,35 +173,35 @@ const PurchaseCosting = () => {
                     </tr>
                   </thead>
                   <tbody>
-                    {costingDetails.map((item, index) => {
+                    {purchaseDetails.map((item, index) => {
                       return (
                         <tr
                           key={item.id}
                           className="border border-gray-600 px-2 py-2"
                         >
                           <td className="text-left px-2 py-1">
-                            {item.STOCK_ITEM}
+                            {item.stock_item}
                           </td>
                           <td className="text-end px-2 py-1">
-                            {numeral(item.QTY).format("0,0")}
+                            {numeral(item.qty).format("0,0")}
                           </td>
                           <td className="text-end px-2 py-1">
-                            {numeral(item.CARTON).format("0,0")}
+                            {numeral(item.carton).format("0,0")}
                           </td>
                           <td className="text-end px-2 py-1">
-                            {numeral(item.RATE).format("0,0.00")}
+                            {numeral(item.rate).format("0,0.00")}
                           </td>
                           <td className="text-end px-2 py-1">
-                            {numeral(item.COST_PRICE).format("0,0.00")}
+                            {numeral(item.cost_price).format("0,0.00")}
                           </td>
                           <td className="text-end px-2 py-1">
-                            {numeral(item.EXPENSES).format("0,0.00")}
+                            {numeral(item.expenses).format("0,0.00")}
                           </td>
                           <td className="text-end px-2 py-1">
-                            {numeral(item.LANDING_COST).format("0,0.00")}
+                            {numeral(item.landing_cost).format("0,0.00")}
                           </td>
                           <td className="text-end px-2 py-1">
-                            {numeral(item.COST_WITH_GST).format("0,0.00")}
+                            {numeral(item.cost_with_gst).format("0,0.00")}
                           </td>
                         </tr>
                       );
@@ -192,7 +224,7 @@ const PendingBills = ({ setSelectedPurchase, setShowPendingBills }) => {
   const [pendingCostingBills, setPendingCostingBills] = useState([]);
 
   useEffect(() => {
-    fetch(`${api}/purchases/pending-costing-bills`, {
+    fetch(`${api}/purchases/pending-purchase-bills`, {
       method: "POST",
     })
       .then((response) => response.json())
@@ -218,14 +250,14 @@ const PendingBills = ({ setSelectedPurchase, setShowPendingBills }) => {
         <table className="w-full">
           <thead className="bg-gray-700 starting:sticky top-8">
             <tr>
-              <th className="text-left px-2 py-1">Voucher Date</th>
-              <th className="text-left px-2 py-1">Voucher No</th>
+              <th className="text-left px-2 py-1">Purchase Date</th>
+              <th className="text-left px-2 py-1">Purchase ID</th>
               <th className="text-left px-2 py-1">Vendor</th>
             </tr>
           </thead>
           <tbody>
             {pendingCostingBills.map((bill, index) => {
-              const key = `${bill.VNO}-${bill.VDT}`;
+              const key = `${bill.purchase_id}-${bill.purchase_date}`;
               return (
                 <tr
                   className="cursor-pointer hover:bg-gray-700/50"
@@ -236,10 +268,10 @@ const PendingBills = ({ setSelectedPurchase, setShowPendingBills }) => {
                   }}
                 >
                   <td className="px-2 py-1">
-                    {moment(bill.VDT).format("DD-MM-YYYY")}
+                    {moment(bill.purchase_date).format("DD-MM-YYYY")}
                   </td>
-                  <td className="px-2 py-1">{bill.VNO}</td>
-                  <td className="px-2 py-1">{bill.VENDOR}</td>
+                  <td className="px-2 py-1">{bill.purchase_id}</td>
+                  <td className="px-2 py-1">{bill.vendor}</td>
                 </tr>
               );
             })}
